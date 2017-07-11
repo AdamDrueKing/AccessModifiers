421ed8d4ef78551faff2aa00d3d7efa5325eac15
Adam King <adam81king@gmail.com>
2017-07-11 15:15:37 -0400

    Access Practice

--- AccessModifiersPractice/Boat.cs
+++ AccessModifiersPractice/Boat.cs
@@ -8,5 +8,25 @@ namespace AccessModifiersPractice
 {
     class Boat
     {
+        //SO far we've done fields as private, but we can do them as public
+        public int numberOfPassengers;
+
+        //Most of the time, though, we want our fields to be private
+        private string name;
+        private string engineSize;
+
+        //Properties almost always have public as their access modifier
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //Constructors always have a public access modifier
+        public Boat()
+        {
+            //this is my default constructor
+        }
+
     }
 }
---
+++ AccessModifiersPractice/CellPhone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersPractice
+{
+    class CellPhone
+    {
+        private string manufacturer;
+            private int diagonalDimensions;
+            private bool touchScreen = true;
+
+        public string Manufacturer
+        {
+            get { return this.manufacturer; }
+            set { this.manufacturer = value; }
+        }
+
+        public bool TouchScreen
+        {
+            get { return this.touchScreen; }
+            set { this.touchScreen = value; }
+        }
+
+        public static void ManufacturerName()
+        {
+            Console.WriteLine("You have a " +  manufacturer + "phone!");
+        }
+    }
+}
